c47b7f69c6b1899937577dff9725bb448f7bb3e7
Qinyue <qinyue.liu@etu.unistra.fr>
2021-10-26 11:16:12 +0200

    stats added

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
 var msg = new Vue({
-    el:"#msg",
+    el:"#begin",
     data:{
         message:"See you again, world!",
+
+        stats:[
+            
+            {stat_name:"Health", value: 100},
+            {stat_name:"Money", value: 7000},
+            {stat_name:"days", value: 1 },
+            {stat_name:"study", value: 10},
+            {stat_name:"energy", value: 50},
+        ],
+    },
+    methods:{
+        study:function(){
+            if (this.stats[4].value >=10){
+                this.message = "Studying..."
+                this.stats[3].value += 5  // study
+                this.stats[4].value -= 10 // energy
+            }
+            else{
+                this.message = "You ran out of energy"
+            }
+        },
+        next_day:function(){
+            this.stats[4].value = 50 // energy
+            this.stats[2].value += 1 // days
+            this.message = "day "+this.stats[2].value
+        }
     }
 })
