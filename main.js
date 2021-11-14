4f19e86c97b0f8aed0105ee85ea5964b8a4cbbfd
Qinyue <qinyue.liu@etu.unistra.fr>
2021-11-14 16:55:36 +0100

    school done

--- main.js
+++ main.js
@@ -2,10 +2,15 @@ var msg = new Vue({
     el:"#begin",
     data:{
         message:"See you again, world!",
-        click_bank:0, click_tag:0,
-        bank_demarche:false, tag_demarche:false,
-        lock_bank:false, lock_ofii:false, lock_caf:false,
-        passport:1, a_naissance:1, attes_logement:1, attes_etudiant:1, photo:1,
+        click_bank:0, click_tag:0, click_school:0,
+        bank_demarche:false, tag_demarche:false, school_demarche:false,
+        lock_ofii:false, lock_caf:false,
+
+        school_weekday:false, tag_done:false,
+        passport:0, a_naissance:0, attes_logement:0, attes_etudiant:0, photo:0,
+
+        be1:false, be2:false,
+        he1:false,
         stats:[
             
             {stat_name:"Health", value: 100},
@@ -21,7 +26,8 @@ var msg = new Vue({
             if (this.stats[4].value >=10){
                 this.message = "Studying..."
                 this.stats[3].value += 5  // study
-                this.stats[4].value -= 10 // energy
+                this.stats[4].value -= 15 // energy
+                this.stats[5].value -= 5 // hanpiness
             }
             else{
                 this.message = "You ran out of energy"
@@ -38,6 +44,30 @@ var msg = new Vue({
                 this.stats[1].value -= 500
             }
             // random events todo
+            // school, bank events
+
+            // if you don't study at school on weekdays, your study will drop
+            if (this.stats[2].value % 6 != 0 && this.stats[2].value % 7 != 0){// if on weekdays
+                if(this.school_weekday == false){
+                    this.stats[3].value -= 7
+                }
+                else{
+                    this.school_weekday = false
+                }
+            }
+            // if you don't do the bank demarche before the 7th day, your hanpiness will drop
+            if (this.stats[2].value >= 7 && this.lock_ofii == false){
+                this.stats[5].value -= 10
+            }
+            // endings
+            if (this.stats[1].value <= 0){
+                this.be1 = true
+                this.message=" You don't have any money to continue your study, you failed the game... "
+            }
+            if (this.stats[3].value <= 25 && this.stats[2].value >= 5){
+                this.be2 = true
+                this.message=" You failed your semester...  GAME OVER"
+            }
         },
         add_object:function(times){
             times += 1
@@ -63,12 +93,16 @@ var msg = new Vue({
             }
         },
         chk_bank:function(){
+            if(this.tag_done == false){
+                this.stats[1].value -= 4 // money for transport
+            }
             if (this.passport == 1 && this.a_naissance == 1 && this.attes_etudiant == 1 && this.attes_logement == 1){
                 this.message = "demarche reussi, study + 10, energy - 10, hanpiness + 10"
                 this.click_bank = -1
                 this.stats[3].value += 5  // study
                 this.stats[4].value -= 10 // energy
                 this.stats[5].value += 10 // hanpiness
+                this.lock_ofii = true
             }
             else{
                 this.message = "T'as pas tous les choses necessaires... reviens quand t'es pret"
@@ -100,11 +134,56 @@ var msg = new Vue({
                 this.stats[3].value += 5  // study
                 this.stats[4].value -= 10 // energy
                 this.stats[5].value += 10 // hanpiness
+                this.tag_done = true
             }
             else{
                 this.message = "T'as pas tous les choses necessaires... reviens quand t'es pret"
             }
             this.tag_demarche = false
         },
+        //---------------------------------------school--------------------------------------
+        intro_school:function(){
+            if(this.tag_done == false){
+                this.stats[1].value -= 4 // money for transport
+            }
+            this.click_school += 1
+            if(this.click_school % 2 != 0){
+                this.message = "The introduction of school..."
+                this.school_demarche = true
+            }
+            else{
+                this.school_demarche = false
+                this.click_school = 0 // Not to get a too big value
+            }
+        },
+        study_school:function(){
+            if (this.stats[4].value == 0){
+                this.message = "You ran out of energy"
+            }
+            else if (this.stats[2].value % 6 != 0 && this.stats[2].value % 7 != 0){ // on weekdays
+                this.message = "studying at school, study + 10, energy - 10, hanpiness - 3"
+                this.stats[3].value += 10  // study
+                this.stats[4].value -= 10 // energy
+                this.stats[5].value -= 3 // hanpiness
+                this.school_weekday = true
+            }
+            else{
+                this.message = " pas d'ecole aujourd'hui "
+            }
+        },
+        socialize_school:function(){
+            if (this.stats[4].value == 0){
+                this.message = "You ran out of energy"
+            }
+            if (this.stats[2].value % 6 != 0 && this.stats[2].value % 7 != 0){ // on weekdays
+                this.message = "socializing at school, study + 2, energy - 10, hanpiness + 15"
+                this.stats[3].value += 2  // study
+                this.stats[4].value -= 10 // energy
+                this.stats[5].value += 10 // hanpiness
+            }
+            else{
+                this.message = " pas d'ecole aujourd'hui "
+            }
+        }
     }
 })
